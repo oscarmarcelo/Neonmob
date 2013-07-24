354ae286e4f27ff2e96daeb0a541a8ba98751a7a
Oscar Marcelo <oscarmarcelofg@hotmail.com>
2013-07-24 21:48:18 +0100

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -2,10 +2,10 @@ var itemGrid, css, head, style;
 
 itemGrid = $("div.collection ul.item-grid");
 
-itemGrid.addClass("om-zoomout");
+itemGrid.toggleClass("om-zoomout");
 
-css = 'om-zoomout .piece.one-quarter .img, om-zoomout .piece.one-quarter img { width: 100px; height: auto; }';
-css += '.om-zoomout .piece.empty .img { height: 96px; -webkit-background-size: 50% 50%; -moz-background-size: 50% 50%; -o-background-size: 50% 50%; background-size: 50% 50%; }';
+css = '.om-zoomout .piece.one-quarter .img, .om-zoomout .piece.one-quarter img { width: 100px; height: auto; }';
+css += '.om-zoomout .piece.empty .img { height: 96px !important; -webkit-background-size: 40px 40px; -moz-background-size: 40px 40px; -o-background-size: 40px 40px; background-size: 40px 40px; }';
 
 head = document.getElementsByTagName('head')[0];
 style = document.createElement('style');
